dab534b7ff770dc87621787919fe75fdb612b241
samade747 <samad.e747@gmail.com>
2024-03-11 01:57:33 +0500

    157

---
+++ weatherappbysamad/src/WeatherCast.jsx
@@ -0,0 +1,26 @@
+import sunPic from "../assets/sun-3.png";
+import Cloud from "../assets/cloud-emoji.png";
+import SunWithCloud from "../assets/sun.png";
+import Sun2 from "../assets/sun-2.png";
+import React from "react";
+import Umbrella from "../assets/umb.png";
+
+
+const WeatherCast = () => {
+    
+    
+
+
+
+
+
+    return(
+        <>
+
+        
+        </>
+
+    )
+}
+
+export default WeatherCast;
