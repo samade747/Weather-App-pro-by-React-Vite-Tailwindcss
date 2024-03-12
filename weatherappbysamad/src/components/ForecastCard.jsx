7f769d64fcd089d5b9b1975098228e17cad83e07
samade747 <samad.e747@gmail.com>
2024-03-12 20:09:09 +0500

    Update ForecastCard.jsx

--- weatherappbysamad/src/components/ForecastCard.jsx
+++ weatherappbysamad/src/components/ForecastCard.jsx
@@ -3,4 +3,12 @@ import { useState } from "react";
 
 
 // Importing image assests 
-import Cloud from 
+import Cloud from "../assets/Cloud-emoji.png"; 
+import SunWithCloud from "../assets/sun.png";
+import Sun2 from "../assets/sun-2.png";
+import Umbrella from "../assets/umb.png";
+import SunPic from "../assets/sun-3.png";
+
+const ForecastCard = {{weatherVal, index}} => {
+
+}
