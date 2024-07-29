c41b2d350b1e6ab1c1471f4f3c3e80b57f2e6434
samade747 <samad.e747@gmail.com>
2024-07-29 14:42:24 +0500

    242

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5173;
 const dbURI = process.env.DB_URI 
 
 // Middleware
